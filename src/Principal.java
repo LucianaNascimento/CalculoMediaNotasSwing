378f7920faccae3d8dcac3af0440b364e67258d1
Luciana Nascimento <luci.nasrika@hotmail.com>
2021-10-15 02:52:50 -0300

    initial commit

---
+++ src/Principal.java
@@ -0,0 +1,10 @@
+
+public class Principal {
+   
+    public static void main(String[] args) {
+        MediaFinal fmedia;
+        fmedia =  new MediaFinal();
+        fmedia.setVisible(true);
+    }
+    
+}
